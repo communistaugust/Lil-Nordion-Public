c7937e2525ec357eb9ef6cff3c12da5eb28646e0
AliScrooge <45091862+AliScrooge@users.noreply.github.com>
2026-09-08 15:59:06 +0200

    Reworked build.bat

--- build.bat
+++ build.bat
@@ -2,6 +2,4 @@ SET startDir=%~dp0
 
 SET as="%startDir%/tools/64tass/64tass"
 
-%as% -f -X -x -o "FE4R.sfc" --vice-labels -l "labels.txt" "buildfile.asm" 1>"log.txt" 2>&1 -Wno-portable
-
-pause
+%as% -f -X -x -o "Game/StraightHair.sfc" --vice-labels -l "labels.txt" "buildfile.asm" 1>"Log/StraightHairlog.txt" 2>&1 -Wno-portable
